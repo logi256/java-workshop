3cdc9bb43696d1beb79310245db3a9f44c2e167b
logi256 <logeshloki428@gmail.com>
2025-07-02 14:19:23 +0530

    updata to git

---
+++ src/com/vetias/java/workshop/basics/Greetings.java
@@ -0,0 +1,9 @@
+package com.vetias.java.workshop.basics;
+
+public class Greetings {
+
+    public static void main(String[] args) {
+        System.out.println("my frist java program");
+    }
+    
+}
