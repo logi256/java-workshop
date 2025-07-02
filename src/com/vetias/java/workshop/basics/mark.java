0d28b9ddea657eabc2cc29a0c323f675237c1ede
logi256 <logeshloki428@gmail.com>
2025-07-02 14:57:22 +0530

    update the code

---
+++ src/com/vetias/java/workshop/basics/mark.java
@@ -0,0 +1,12 @@
+package com.vetias.java.workshop.basics;
+
+public class mark {
+
+    public static void main(String[] args) {
+        int[]marks={50,80,87,90,60};
+        for (int mark:marks){
+            System.out.println(mark);
+        }
+    }
+    
+}
---
+++ src/com/vetias/java/workshop/basics/total.java
@@ -0,0 +1,14 @@
+package com.vetias.java.workshop.basics;
+
+public class total {
+
+    public static void main(String[] args) {
+        int a=50,b=76,c=85,d=65,e=80;
+
+        int tot = a+b+c+d+e;
+
+         System.out.println("tot:"+tot);
+
+    }
+    
+}
