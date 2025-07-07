dc7f79d57b62e2929523f61119624ac92b1db225
logi256 <logeshloki428@gmail.com>
2025-07-07 15:56:04 +0530

    new

---
+++ src/com/vetias/java/workshop/temp/bin/StringTokenizerexample.java
@@ -0,0 +1,17 @@
+import java.util.StringTokenizer;
+
+public class StringTokenizerexample {
+
+    public static void main(String[] args) {
+        System.out.println("i learned java code");
+        String str = "Hello, how are you? I hope you are doing well.";
+        StringTokenizer tokenizer = new StringTokenizer(str, " ,?");
+
+        System.out.println("Total tokens: " + tokenizer.countTokens());
+
+        while (tokenizer.hasMoreTokens()) {
+            System.out.println(tokenizer.nextToken());
+        }   
+    }
+
+}
