f9e50060d3dea794f48f5f1f09effdfa38254eae
logi256 <logeshloki428@gmail.com>
2025-07-07 15:35:15 +0530

    update

---
+++ src/com/vetias/java/workshop/temp/bin/Namegenrater.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Namegenrater {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            System.err.println("Enter your gender: ");
+            String gender = scanner.nextLine();
+            System.out.println("Enter your qualification: ");
+            String qualification = scanner.nextLine();
+            if("male".equalsIgnoreCase(gender)) {
+                System.out.println(" Mr. " + name + ", tittle " + qualification);
+            } else {
+                System.out.println(" Ms. " + name + ", title " + qualification);
+            }
+        }
+        }
+    }
