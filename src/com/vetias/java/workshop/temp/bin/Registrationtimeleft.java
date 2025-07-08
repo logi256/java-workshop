b9eb8428ce3f9766dc663262fc297dbaa4c3a844
logi256 <logeshloki428@gmail.com>
2025-07-08 10:51:50 +0530

    new

---
+++ src/com/vetias/java/workshop/temp/bin/Registrationtimeleft.java
@@ -0,0 +1,16 @@
+package com.vetias.java.workshop.temp.bin;
+
+import java.time.LocalTime;
+
+public class Registrationtimeleft {
+
+    public static void main(String[] args) {
+    LocalTime currentTime = LocalTime.now();
+    LocalTime registrationEndTime = LocalTime.of(17, 30, 30);
+    int hoursLeft = registrationEndTime.getHour() - currentTime.getHour();
+    int minutesLeft = registrationEndTime.getMinute() - currentTime.getMinute();
+    int secondsLeft = registrationEndTime.getSecond() - currentTime.getSecond();
+    System.out.println("you have " + hoursLeft + " hours, " + minutesLeft + " minutes, and " + secondsLeft + " seconds left to register.");
+    }
+
+}
